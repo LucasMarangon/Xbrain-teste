11026cfc663962054eb7267d582215c6634c1b40
Lucas Marangon <157633640+LucasMarangon@users.noreply.github.com>
2024-01-24 16:28:39 -0300

    Add files via upload

---
+++ src/main/java/br/com/springbootXB/SpringbootXbApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbootXB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootXbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootXbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springbootXB/SpringbootXbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springbootXB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootXbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
