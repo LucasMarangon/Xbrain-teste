77e94af488625c7e59ce6d722510a40373cbc62b
Lucas Marangon <lucasmarangon.tabaquini@gmail.com>
2024-01-24 18:19:08 -0300

    Adicionados os models dos usuários e de vendas, adicionado o repositório para Vendas, ainda sem ser finalizado

---
+++ src/main/repository/VendaRepository.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendaRepository extends JpaRepository<Venda, Long>{
+
+@Query("")
+    
+} 
+    
+    
+
+
